d57e2e40f6f29319039f22b26d9cf858fb0470a8
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-09 08:45:51 -0400

    Add tests.

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -3,12 +3,17 @@ import { mount } from 'enzyme';
 import { EmptyState, Button } from '@shopify/polaris';
 import App from '../components/App';
 import Form from '../components/Form';
+import UserCard from '../components/UserCard';
+import ErrorBanner from '../components/Error';
 
-// const setup = () => {
-//   return 
-// };
+import { users, request } from './request';
+jest.mock('./mock.request');
 
-describe('Using the app for the first time', () => {
+const addUser = (userName: string) => {
+  return request(`https://api.github.com/users/${userName}`);
+};
+
+describe('using the app for the first time', () => {
   let app;
   beforeEach(() => {
     app = mount(<App />);
@@ -25,5 +30,32 @@ describe('Using the app for the first time', () => {
     expect(app.find(Form).length).toEqual(0);
     emptyStateBtn.simulate('click');
     expect(app.find(Form).length).toEqual(1);
+  });
+});
+
+describe('adding users', () => {
+  let app;
+  let form;
+  beforeEach(() => {
+    app = mount(<App />);
+    app.find(Button).at(0).simulate('click');
+    expect(app.find(Form).length).toEqual(1);
+    form = app.find(Form).at(0);
+  });
+
+  test('the mock api returns the correct user', () => {
+    return addUser('anveio').then(data => expect(data).toEqual(users.anveio));
+  });
+
+  // test('the form works', () => {
+  //   form.simulate('keyDown');
+  //   expect(app.find(ErrorBanner).length).toEqual(1);
+  // });
+
+  test('the mock api returns an axios error for an invalid user', () => {
+    return addUser('hfajsnfkasnf')
+      .then(data => 
+        expect(data)
+        .toThrow('Could not find user with name: \'hfajsnfkasnf\''));
   });
 });
